487696c28e5e4f20687242947d31381862a8d36d
Aidar <65452919+Aidar05@users.noreply.github.com>
2023-04-27 17:00:25 +0600

    Update main.py
    Убрал лишнее. Добавил коментарии

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from kivy.app import App
+# Импорт нужных частей модуля  
+from kivy.app import App 
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.config import Config
 from kivy.lang import Builder
@@ -6,57 +7,64 @@ from kivy.uix.popup import Popup
 from kivy.uix.checkbox import CheckBox
 
 
-# on_touch_down: app.Btn_Press_Filter(*args)
-
+# Описываю ширину и высоту окна приложения 
 root_width = 330
 root_height = 580 
 
 
+# Меняю размеры окна ранее выбранные 
 Config.set('graphics', 'width', root_width)
 Config.set('graphics', 'height', root_height)
 
 
+# Класс для главного экрана
 class Main(Screen):
+    # Инициализация класса. На самом деле не уверен если необходима
     def __init__(self, **kw):
         super().__init__(**kw)
 
 
+# Класс для экрана с ноутбуками.  
 class Notebooks(Screen):
+    # Еще одна инициализация класса
     def __init__(self, **kw):
         super().__init__(**kw)
 
 
+# Класс чтобы мэнэджить все окна
 class WindowsManager(ScreenManager):
     pass
 
 
+# Класс для выпадающего окошка. Фильтр пойска 
 class FilterWindow(Popup):
     pass
 
 
+# Класс моего приложения 
 class StoreApp(App):
+    # Необходимый метод билд который тригериться в начале приложния
     def build(self):
+        # Создаем instance менеджера скринов и добавляем в него все окна
         self.screen_manager = ScreenManager()
         self.screen_manager.add_widget(Main(name='main'))
         self.screen_manager.add_widget(Notebooks(name='notebooks'))
-        return self.screen_manager
+        return self.screen_manager    # Возвращаем главный экран
     
-    # def Btn_Press_Filter(self, instance, touch):
-    #     if instance.collide_point(*touch.pos):
-    #         if self.screen_manager.current == 'main':
-    #             self.screen_manager.current = 'notebooks'
-    #         elif self.screen_manager.current == 'notebooks':
-    #             self.screen_manager.current = 'main'
-
+    # Метод для открывания фильтра поиска. Тригериться при нажатия на кнопку в киви 
     def OpenFilter(self):
         self.popup = FilterWindow()
         self.popup.open()
 
+    # Закрывает фильтр окошко. Срабатывает при нажатии на подходящую кнопку в киви 
     def CloseFilter(self):
         self.popup.dismiss()
 
+
+# Загружаем киви файл 
 kv = Builder.load_file('kivy.kv')
 
 
+# Срабатывает только если он запускается как главный процесс на прямую
 if __name__ == '__main__':
     StoreApp().run()
