1dc3d0b4619c334d0419eb6b36266bf6676d3929
Aidar <65452919+Aidar05@users.noreply.github.com>
2023-05-27 23:19:41 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,125 +1,187 @@
-# Импорт нужных частей модуля
-import kivymd  
-from kivymd.app import MDApp 
-from kivy.uix.screenmanager import Screen, ScreenManager
-from kivy.config import Config
-from kivy.lang import Builder
-from kivy.uix.popup import Popup
-from kivy.properties import ObjectProperty
-from kivy.core.window import Window
+from kivymd.app import MDApp
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder 
+from kivymd.uix.list import OneLineListItem
+from kivy.properties import StringProperty
+
 
 import mysql.connector
 
 
-db = mysql.connector.connect(user='root',
-                              password='',
-                              host='localhost',
-                              database='user_data')
+db = mysql.connector.connect(user='root', password='',
+                             host='localhost', database='mess')
+
+db_second = mysql.connector.connect(user='root', password='',
+                                    host='localhost', database='user_account_data')
+
+count = 2
+
+class SpeedType(Screen):
+    def game_start(self):
+        self.ids.speedtype.data = []
+
+        global count
+
+        def add_user_name(word):
+            self.ids.speedtype.data.append(
+                {
+                    "viewclass": "CustomOneLineIconListItem",
+                    "text": word,
+                    "callback": lambda x: x,
+                }
+            )
+
+        cursor = db.cursor()
+
+        cursor.execute('select word from words')
+
+        words = cursor.fetchall()
+
+        for i in range(count):
+            print(words[i][0])
+            add_user_name(words[i][0])
+        count+=2
 
 
-# Описываю ширину и высоту окна при ложения 
-root_width = 330
-root_height = 580 
-
-
-# Меняю размеры окна ранее выбранные
-Window.size = (root_width, root_height)
-
-
-# Класс для главного экрана
-class Main(Screen):
+class Profile(Screen):
     pass
 
-# Класс для экрана с ноутбуками.  
-class Friends(Screen):
+
+class CustomOneLineIconListItem(OneLineListItem):
+    icon = StringProperty()
+
+
+class Main(Screen):    
     pass
+
+
+class SignIn(Screen):
+    def check_account(self):
+        data = [self.ids.login.text, self.ids.password.text]
+
+        cursor = db_second.cursor()
+
+        cmd = 'select email, pass from data '
+
+        cursor.execute(cmd)
+
+        result = cursor.fetchall()
+
+        count = 0
+        for i in result:
+            if i[0] == data[0]:
+                count += 1
+                if i[1] == data[1]:
+                    print('logged in')
+                    break
+                else: 
+                    self.ids.password.error = True
+            else: 
+                pass
+        
+        if count == 0:
+            self.ids.login.error = True
 
 
 class SignUp(Screen):
-    def user_data_insert(self):
-        user_data = [self.ids.user_name.text,
-                     self.ids.user_last_name.text,
-                     self.ids.user_login_number.text,
-                     self.ids.user_password.text
-                    ]
-        print(user_data)
+    def create_account(self):
+        user_data = [self.ids.name.text, self.ids.last_name.text,
+                    self.ids.gender.text, self.ids.age.text,
+                    self.ids.login.text, self.ids.password.text] 
 
-        cursor = db.cursor()
+        cursor = db_second.cursor() 
 
-        cmd = 'insert into data (name, last_name, login, password) values (%s, %s, %s, %s)'
-
+        cmd = 'insert into data (name, surname, gender, age, email, pass) values (%s, %s, %s, %s, %s, %s)'
+    
         cursor.execute(cmd, user_data)
 
-        db.commit()
+        db_second.commit()
 
         cursor.close()
 
-class SignIn(Screen):
-    def user_data_check(self):
-        cursor = db.cursor()
 
-        cmd = 'select * from data'
-
-        cursor.execute(cmd)
-        data = cursor.fetchall()
-
-        for i in data:
-            print(f'login,  {i[3]}')
-            print(f'password,  {i[4]}')
-
-        cursor.close()
-        print('Checking...')
-
-
-# Класс чтобы мэнэджить все окна
-class WindowsManager(ScreenManager):
+class Profile(Screen):
     pass
 
 
-# Класс для выпадающего окошка. Фильтр пойска 
-class FilterWindow(Popup):
-    pass        
+class Settings(Screen):
+    pass
 
 
-# Класс моего приложения 
-class StoreApp(MDApp):
-    # Необходимый метод билд который тригериться в начале приложния
+class Chosen(Screen):
+    pass
+
+
+# class MusNote(Screen):
+#     pass
+
+class Settings(Settings):
+    pass
+
+
+class MessApp(MDApp):
     def build(self):
-        self.theme_cls.primary_palette = 'BlueGray'
+        self.theme_cls.primary_palette = 'DeepOrange'
 
-        # Создаем instance менеджера скринов и добавляем в него все окна
-        self.screen_manager = ScreenManager()
-        self.screen_manager.add_widget(SignUp(name='sign_up'))
-        self.screen_manager.add_widget(SignIn(name='sign_in'))
-        self.screen_manager.add_widget(Main(name='main'))
-        self.screen_manager.add_widget(Friends(name='notebooks'))
-        return self.screen_manager    # Возвращаем главный экран
+        self.main = Main()
+
+        self.sc = ScreenManager()
+        # self.sc.add_widget(MusNote(names='MusNote'))
+        self.sc.add_widget(Main(name='main'))
+        self.sc.add_widget(SpeedType(name='SpeedType'))
+        self.sc.add_widget(Chosen(name='chosen'))
+        self.sc.add_widget(Profile(name='profile'))
+        self.sc.add_widget(SignUp(name='sign_up'))
+        self.sc.add_widget(SignIn(name='sign_in'))
+        # self.sc.add_widget(Settings(name='settings'))
+        return self.sc
+
+    def theme_yellow(self):
+        self.theme_cls.primary_palette = 'LightGreen'
+
+    def screen_main(self):
+        self.sc.current = 'main'
+
+    def screen_main_s(self, instance, touch):
+        if instance.collide_point(*touch.pos):
+            self.sc.current = 'main'
+
+    def screen_sign_in(self):
+        self.sc.current = 'sign_in'
+
+    def screen_sign_in_s(self, instance, touch):
+        if instance.collide_point(*touch.pos):
+            self.sc.current = 'sign_in'
+
+    def screen_sign_up(self):
+        self.sc.current = 'sign_up'
+
+    def screen_sign_up_s(self, instance, touch):
+        if instance.collide_point(*touch.pos):
+            self.sc.current = 'sign_up'
+
+    def screen_SpeedType(self, instance, touch):
+        if instance.collide_point(*touch.pos):
+            self.sc.current = 'SpeedType'
+
+    def screen_chosen(self, instance, touch):
+        if instance.collide_point(*touch.pos):
+            self.sc.current = 'chosen'
+
+    def screen_profile(self):
+        self.sc.current = 'profile'
+
+    def screen_settings(self):
+        pass
     
-    def MainScreen(self):
-        self.screen_manager.current = 'main'
+kv = Builder.load_file('kivy_files/main.kv')
+# kv = Builder.load_file('kivy_files/MusNote.kv')
+kv = Builder.load_file('kivy_files/sign_in.kv')
+kv = Builder.load_file('kivy_files/sign_up.kv')
+kv = Builder.load_file('kivy_files/profile.kv')
+kv = Builder.load_file('kivy_files/settings.kv')
+kv = Builder.load_file('kivy_files/asdf.kv')
+kv = Builder.load_file('kivy_files/speedtype.kv')
 
-    # Метод для открывания фильтра поиска. Тригериться при нажатия на кнопку в киви 
-    def OpenFilter(self, instance, touch):
-        # Окно фильтр открываеться только если нажали на саму кнопку 
-        if instance.collide_point(*touch.pos):
-            self.popup = FilterWindow()
-            self.popup.open()
-
-    # Закрывает фильтр окошко. Срабатывает при нажатии на подходящую кнопку в киви 
-    def CloseFilter(self):
-        self.popup.dismiss()
-
-
-    def Profile(self, instance, touch):
-        if instance.collide_point(*touch.pos):
-            # change screen 
-            self.screen_manager.current = 'sign_in'
-
-# Загружаем киви файл 
-kv = Builder.load_file('kivy.kv')
-kv = Builder.load_file('Sign_in.kv')
-kv = Builder.load_file('Sign_up.kv')
-
-# Срабатывает только если он запускается как главный процесс на прямую
 if __name__ == '__main__':
-    StoreApp().run()
+    MessApp().run()
